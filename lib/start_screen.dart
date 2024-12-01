b889a875902ba894d105882dbfa57d017bcd37c0
Ddayin <ddai.develop@gmail.com>
2024-12-01 19:07:59 +0900

    add image_select_screen

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -1,6 +1,6 @@
+import 'package:edit_snap/image_select_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:intl/intl.dart';
 
 class StartScreen extends StatelessWidget {
   const StartScreen({super.key});
@@ -24,7 +24,12 @@ class StartScreen extends StatelessWidget {
             textAlign: TextAlign.center,
           ),
           ElevatedButton(
-            onPressed: () {},
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(builder: (context) => const ImageSelectScreen(),
+                ),
+              );
+            },
             child: Text(l10n.start),
           )
         ],
